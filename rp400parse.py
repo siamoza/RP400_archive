56d20c51a3f04881d69a423c3809c6da0e784af3
Siamoza <siamoza@gmail.com>
2022-12-01 10:03:42 +0300

    0.4.3
    rp400parse.py: подключено распознавание русского языка. Скорректирован регион распознавания из-за периодических посторонних артефактов на скриншотах.

--- rp400parse.py
+++ rp400parse.py
@@ -7,12 +7,12 @@ from PIL import Image
 import pandas as pd
 from datetime import datetime
 
-SOURCE = '/opt/datasets/rp400'
-DATASET = '/opt/datasets/rp400/ocr.txt'
+SOURCE = '/opt/datasets/rp400/172.16.41.108/usbdisk/disk_a_1/screenshots'
+DATASET1 = '/opt/datasets/rp400/dataset1.txt'
 LEFT_X = 32
 LEFT_Y = 388
-RIGHT_X = 738
-RIGHT_Y = 419
+RIGHT_X = 743
+RIGHT_Y = 418
 
 if __name__ == '__main__':
     files = os.listdir(SOURCE)
@@ -21,10 +21,14 @@ if __name__ == '__main__':
         string = datetime.fromtimestamp(os.path.getmtime(fullname)).strftime('%Y-%m-%d %H:%M:%S').split(' ')
         image = Image.open(fullname)
         image_cropped = image.crop((LEFT_X, LEFT_Y, RIGHT_X, RIGHT_Y))
-        st = pytesseract.image_to_string(image_cropped).split(" ")
-        string.append(st[0])
-        string.append(st[1])
+        st = pytesseract.image_to_string(image_cropped, lang='rus').split(" ")
+        print("Файл ", x, "распознана строка:", st)
+        first = "null" if st[0] == "" else st[0]
+        second = "null" if st[1] == "" else st[1]
+        string.append(first)
+        string.append(second)
+        # string.append(st)
         string.append(x)
-        string = [line.rstrip() for line in string]
+        # string = [line.rstrip() for line in string]
 
         print(string)
