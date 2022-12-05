1005d0fdd1cc91e1778ed0a09b7f694703ccc542
Siamoza <siamoza@gmail.com>
2022-12-05 09:43:19 +0300

    0.4.6
    rp400filter.py: настроена фильтрация. Релиз.

--- rp400filter.py
+++ rp400filter.py
@@ -10,3 +10,21 @@ if __name__ == '__main__':
     p = pd.read_csv(SOURCE, dtype=str, skipinitialspace=True, delimiter=',')
     p.columns = ["date", "time", "axis", "worker"]
     p_stat = "До зачистки " + str(len(p)) + " строк."
+
+    p['worker'].replace(to_replace=r'\'', value='', regex=True, inplace=True)
+    p['date'].replace(to_replace=r'^\[', value='', regex=True, inplace=True)
+    p['worker'].replace(to_replace=r'\]$', value='', regex=True, inplace=True)
+    p['worker'].replace(to_replace=r'БОЕС', value='БОЕВ', regex=True, inplace=True)
+    p['worker'].replace(to_replace=r'БУЛГАК.*', value='БУЛГАКОВ', regex=True, inplace=True)
+    p['worker'].replace(to_replace=r'ЮОЕВ', value='БОЕВ', regex=True, inplace=True)
+    p['worker'].replace(to_replace=r'ВОРОНК.*', value='ВОРОНКОВ', regex=True, inplace=True)
+    p['worker'].replace(to_replace=r'КУЗНЕЦ.*', value='КУЗНЕЦОВ', regex=True, inplace=True)
+    p['worker'].replace(to_replace=r'КУКШИН.*', value='КУКШИНОВ', regex=True, inplace=True)
+    p['worker'].replace(to_replace=r'ЛУНЕВ', value='ЛУНЁВ', regex=True, inplace=True)
+    p['worker'].replace(to_replace=r'ТИБЕКИ.*', value='ТИБЕКИН', regex=True, inplace=True)
+    p['worker'].replace(to_replace=r'ХОРХОР.*', value='ХОРХОРДИН', regex=True, inplace=True)
+    p['worker'].replace(to_replace=r'ЧЕРНИК.*', value='ЧЕРНИКОВ', regex=True, inplace=True)
+    p['worker'].replace(to_replace=r'ШКАБАР.*', value='ШКАБАРНЯ', regex=True, inplace=True)
+
+    p.sort_values('date')
+    p.to_csv(FILTERED, index=False)
